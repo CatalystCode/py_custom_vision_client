38697208e748b56e5b5c21d90bd3c0f445d5f6a4
Clemens Wolff <clewolff@microsoft.com>
2017-10-16 10:20:00 -0400

    Release new version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ packages = find_packages(exclude=['tests*'])
 
 setup(
     name='custom_vision_client',
-    version='0.0.5',
+    version='0.0.6',
     author='Clemens Wolff',
     author_email='clewolff@microsoft.com',
     packages=packages,
